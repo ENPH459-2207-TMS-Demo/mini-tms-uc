cc2dbc60dd88e8d6c006f732a9172065fcb8c390
Charles Lee <charles.lee@ubcorbit.com>
2022-03-29 17:24:56 -0700

    added buzzer functionalities, fully tested button switching for mode changing

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,16 @@ For kHz range: use value/100*5 for desired value
 Enter the floating point exactly: do not rely on microcontroller operation for the math
 */
 #define driver_period 1.76056338028169014 // approximately 283 kHz
-#define proto1 1000000
-#define proto10 10000000
+#define proto10 50000
+#define proto1 500000
 
 //Display disp;
 
-byte tms_state = false;
 byte pwm1 = false;
 byte pwm2 = true;
 byte sd_state = true;
+
+uint8_t tms_state = 0;
 
 // Create an IntervalTimer object 
 IntervalTimer pwmTimer;
@@ -30,21 +31,14 @@ IntervalTimer tmsTimer;
 
 const int ledPin = LED_BUILTIN;  // the pin with a LED
 
-// TODO: Test these buttons
-const int button_const = 11;
-const int button_1hz = 12;
-const int button_10hz = 13;
+const int control = 11;
 
 // Timer pins
 const int reg_HIGH = 3;
 const int reg_LOW = 4;
 
 // Buzzer pin
-const int buzzer = 9;
-
 const int SD_pin = 6; // SD pin for gate drivers
-
-const int power = 34;
 
 void gate_driver() {
   digitalWriteFast(reg_LOW, pwm1);
@@ -56,87 +50,56 @@ void gate_driver() {
 void tms_protocol() {
   digitalWriteFast(SD_pin, sd_state);
   sd_state = !sd_state;
+  if (sd_state) {
+    tone(35, 264);
+  } else {
+    noTone(35);
+  }
 }
-
-// Code below is used for ISR implementation of buttons (debouncing done outside)
-// void ISR_const(){
-//   tmsTimer.end();
-// }
-
-// void ISR_1hz(){
-//   tmsTimer.begin(tms_protocol,proto1);
-// }
-
-// void ISR_10hz(){
-//   tmsTimer.begin(tms_protocol,proto10);
-// }void ISR_const(){
-//   tmsTimer.end();
-// }
-
-// void ISR_1hz(){
-//   tmsTimer.begin(tms_protocol,proto1);
-// }
-
-// void ISR_10hz(){
-//   tmsTimer.begin(tms_protocol,proto10);
-// }
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(reg_HIGH, OUTPUT);
   pinMode(reg_LOW, OUTPUT);
 
-  pinMode(power, INPUT_PULLDOWN);
+  pinMode(35,OUTPUT);
 
-  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+
+  //pinMode(buzzer, OUTPUT);
 
   // Thing
   pinMode(SD_pin, OUTPUT);
+  pinMode(control, INPUT_PULLUP);
   
   pwmTimer.begin(gate_driver, driver_period); // 5 is 100 kHz, adjust accordingly
   tmsTimer.priority(100);
-  // disp.setup();
-  // TODO: below is inplementation of interrupt pins
-  // attachInterrupt(digitalPinToInterrupt(button_const), ISR_const,RISING);
-  // attachInterrupt(digitalPinToInterrupt(button_1hz), ISR_1hz,RISING);
-  // attachInterrupt(digitalPinToInterrupt(button_10hz), ISR_10hz,RISING);
 }
 
-// TODO: test this; debouncing coded at 10 ms atm
-Bounce constButton = Bounce(button_const, 10);
-Bounce oneButton = Bounce(button_1hz, 10);
-Bounce tenButton = Bounce(button_10hz, 10);
+Bounce button = Bounce(control, 10);
 
 void loop() {
-  volatile byte state_copy; // copy of sd_state
-  if (constButton.update()){
-    if (constButton.fallingEdge()) { // this is for pin button to gnd, pressed
-      //disp.println("TMS Protocol: OFF");
-      state_copy = false;
-      tmsTimer.end();
+  Serial.print("Current state: ");
+  Serial.println(tms_state);
+  if (button.update()){
+    if (button.fallingEdge()){
+      tms_state++;
+      if (tms_state == 0){
+        // tmsTimer.end();
+        Serial.println("TMS: Const");
+      } else if (tms_state ==1){
+        tmsTimer.begin(tms_protocol, proto1);
+        Serial.println("TMS: 1Hz");
+      } else if (tms_state == 2) {
+        tmsTimer.update(proto10);
+        Serial.println("TMS: 10Hz");
+      } else {
+        tms_state = 0;
+        tmsTimer.end();
+        digitalWriteFast(SD_pin, 0); // turn off SD pin
+        noTone(35);
+      }
     }
-  } else if (oneButton.update()){
-    if (oneButton.fallingEdge()){
-      //disp.println("TMS Protocol: 1 Hz");
-      tmsTimer.begin(tms_protocol,proto1);
-    }
-  } else if (tenButton.update()){
-    if (tenButton.fallingEdge()){
-      //disp.println("TMS Protocol: 10 Hz");
-      tmsTimer.begin(tms_protocol,proto10);
-    }
-  }
-  //disp.clear();
-  
-  // TODO: test if this causes some delay in the uc that is non-negligible
-  noInterrupts();
-  state_copy = sd_state;
-  interrupts();
-
-  if (state_copy){
-    analogWriteFrequency(buzzer, 1000);
-  } else {
-    analogWriteFrequency(buzzer, 0);
   }
 }
 
